b2225593cf2e97715864f7eb6f67264476769b95
ricoi <ricoisdenaam@gmail.com>
2025-05-25 18:17:33 +0200

    Frontpage updated, and added additional animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {ServicestatusComponent} from './servicestatus/servicestatus.component';
+import {KeyValuePipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ServicestatusComponent],
+  imports: [RouterOutlet, ServicestatusComponent, NgForOf, KeyValuePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'statusdashfrontend';
+  public services: Record<string, Service> = {};
+
+  constructor() {
+    fetch('config/services.json')
+      .then(response => response.json())
+      .then(json => {
+        this.services = json;
+      });
+  }
+
+  protected readonly String = String;
+}
+
+
+interface Service {
+  name: string;
+  fields: { [key: string]: string };
+  description: string;
+  url: string;
 }
--- src/app/servicestatus/servicestatus.component.ts
+++ src/app/servicestatus/servicestatus.component.ts
@@ -1,33 +1,38 @@
-import {Component, inject, input} from '@angular/core';
+import {Component, inject, Input, input} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map, Observable, Subject, switchMap, takeWhile, tap, timer} from 'rxjs';
 import {readableStreamLikeToAsyncGenerator} from 'rxjs/internal/util/isReadableStreamLike';
-import {AsyncPipe, DatePipe, NgClass} from '@angular/common';
+import {AsyncPipe, DatePipe, NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-servicestatus',
   templateUrl: './servicestatus.component.html',
   styleUrl: './servicestatus.component.css',
   imports: [
-    NgClass
+    NgClass,
+    NgIf
   ],
   providers: []
 })
 
 
+
 export class ServicestatusComponent {
   private http = inject(HttpClient)
-  status = 'Not Checked';
+  status = 'Background service is not running';
   last_updated_at = '';
-
-  service = input('banwatch');
+  selected = false;
+  @Input() service!: Service;
 
 
   getstatus(): any {
     // Make a GET request to the API endpoint
-    this.http.post<any>(`http://127.0.0.1:8000/api/${this.service()}`, {}).subscribe(data => {
+    console.log("Refreshing status for service: " + this.service.name)
+    console.log(this.service.name)
+    this.http.post<any>(`http://127.0.0.1:8000/api/${this.service.name}`, {}).subscribe(data => {
       this.status = data.status;
       this.setdate()
+
     })
   }
 
@@ -52,4 +57,10 @@ export class ServicestatusComponent {
   }
 
   protected readonly readableStreamLikeToAsyncGenerator = readableStreamLikeToAsyncGenerator;
+}
+interface Service {
+  name: string;
+  fields: { [key: string]: string };
+  description: string;
+  url: string;
 }
