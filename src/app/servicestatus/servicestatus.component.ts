4d9bd989159c634fe5d76392a83026ea50372977
wolfricoz <ricoisdenaam@gmail.com>
2025-05-26 12:52:08 +0200

    css updated, now phone compatitable and bug fixed

--- src/app/servicestatus/servicestatus.component.ts
+++ src/app/servicestatus/servicestatus.component.ts
@@ -29,7 +29,12 @@ export class ServicestatusComponent {
     // Make a GET request to the API endpoint
     console.log("Refreshing status for service: " + this.service.name)
     console.log(this.service.name)
-    this.http.post<any>(`api/status/${this.service.name}`, {}).subscribe(data => {
+    // this.http.post<any>(`api/status/${this.service.name}`, {}).subscribe(data => {
+    //   this.status = data.status;
+    //   this.setdate()
+    //
+    // })
+        this.http.post<any>(`https://status.roleplaymeets.com/api/status/${this.service.name}`, {}).subscribe(data => {
       this.status = data.status;
       this.setdate()
 
